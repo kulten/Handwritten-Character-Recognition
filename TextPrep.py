fbe773a4775e5d3ebec81377c5f5fb21774f5ad1
Delvin Cruze <delvinc82@gmail.com>
2018-06-08 14:54:28 +0530

    removed console status messages

--- TextPrep.py
+++ TextPrep.py
@@ -309,11 +309,8 @@ class ImgPrep:
            13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H', 18: 'I', 19: 'J', 20: 'K', 21: 'L', 22: 'M', 23: 'N',
            24: 'O', 25: 'P', 26: 'Q', 27: 'R', 28: 'S', 29: 'T', 30: 'U', 31: 'V', 32: 'W', 33: 'X', 34: 'Y',
            35: 'Z', 36: 'a', 37: 'b', 38: 'd', 39: 'e', 40: 'f', 41: 'g', 42: 'h', 43: 'n', 44: 'q', 45: 'r', 46: 't'}
-        print('2')
         a = self.seperateAnalyse(img)
-        print('3')
         str1 = ""
-        print('4')
         le = len(a)
         x = neural(a,le)
         #print("shape ",a.shape)
